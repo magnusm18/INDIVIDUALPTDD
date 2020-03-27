7fcb93f2bed3381fb06db55002ee08dbbb9d93a7
magnusm18 <magnusm18@ru.is>
2020-03-27 13:33:57 +0000

    red test and refactoring to allow newline input

--- test_kata.py
+++ test_kata.py
@@ -9,7 +9,7 @@ def kata(number):
 
     for i in number:
         sub_str += i
-        if i == ',':
+        if i == ',' or i == "\n":
             sub_str = ""
         else:
             if len(sub_str) > 1:
@@ -21,4 +21,4 @@ def kata(number):
 
 
 def test_kata():
-    assert kata("10,2,5,22,1,1") == 15
+    assert kata("1\n2,3") == 15
