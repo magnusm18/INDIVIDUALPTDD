85920fac288b97edeea76b869fde994d4fe229da
magnusm18 <magnusm18@ru.is>
2020-03-27 12:36:47 +0000

    Error during collection

--- test_kata.py
+++ test_kata.py
@@ -2,5 +2,8 @@ def kata(number):
     if number == "":
         return 0
 
+    elif len(number) == 1:
+        return (int(number)
+
 def test_kata():
-    assert kata("") == 0
+    assert kata("1") == 0
