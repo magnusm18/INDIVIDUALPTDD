82072856897d7cd85984cbfdc428da4c11185d23
magnusm18 <magnusm18@ru.is>
2020-03-27 12:40:33 +0000

    red test

--- test_kata.py
+++ test_kata.py
@@ -2,8 +2,8 @@ def kata(number):
     if number == "":
         return 0
 
-    elif len(number) == 1:
-        return (int(number)
+    if len(number) == 1:
+        return int(number)
 
 def test_kata():
     assert kata("1") == 0
